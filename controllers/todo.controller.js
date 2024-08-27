9dc820a469b8590b645a56361088dec9488a072e
Neha Muley <nehamuley955@gmail.com>
2024-08-27 12:58:10 +0530

    databse setup

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,14 +1,19 @@
 const asyncHandler = require("express-async-handler")
+const Todo = require("../model/Todo")
 
 exports.createTodo = asyncHandler(async (req, res) => {
+    const result = await Todo.create(req.body)
     res.json({ message: "createTodo Success" })
 })
 exports.readTodo = asyncHandler(async (req, res) => {
-    res.json({ message: "readTodo Success" })
+    const result = await Todo.find()
+    res.json({ message: "readTodo Success" ,result})
 })
 exports.updateTodo = asyncHandler(async (req, res) => {
+    const result = await Todo.findByIdAndUpdate(req.params.id, req.body)
     res.json({ message: "updateTodo Success" })
 })
 exports.deleteTodo = asyncHandler(async (req, res) => {
+    const result = await Todo.findByIdAndUpdate(req.params.id)
     res.json({ message: "deleteTodo Success" })
 })
