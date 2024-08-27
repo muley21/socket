23eebbf74de54c220f73d2a47b71504f72c319ad
Neha Muley <nehamuley955@gmail.com>
2024-08-27 13:35:30 +0530

    delete

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -7,13 +7,13 @@ exports.createTodo = asyncHandler(async (req, res) => {
 })
 exports.readTodo = asyncHandler(async (req, res) => {
     const result = await Todo.find()
-    res.json({ message: "readTodo Success" ,result})
+    res.json({ message: "readTodo Success", result })
 })
 exports.updateTodo = asyncHandler(async (req, res) => {
     const result = await Todo.findByIdAndUpdate(req.params.id, req.body)
     res.json({ message: "updateTodo Success" })
 })
 exports.deleteTodo = asyncHandler(async (req, res) => {
-    const result = await Todo.findByIdAndUpdate(req.params.id)
+    const result = await Todo.findByIdAndDelete(req.params.id)
     res.json({ message: "deleteTodo Success" })
 })
